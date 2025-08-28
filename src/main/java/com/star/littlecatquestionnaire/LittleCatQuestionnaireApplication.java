a84be66a97a5cb411f8f6b5f74a3eacac95838ce
yxb123456 <2186256471@qq.com>
2025-08-28 14:48:28 +0800

    feat(project): 初始化项目

---
+++ src/main/java/com/star/littlecatquestionnaire/LittleCatQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.star.littlecatquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleCatQuestionnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleCatQuestionnaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/littlecatquestionnaire/LittleCatQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.littlecatquestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleCatQuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
